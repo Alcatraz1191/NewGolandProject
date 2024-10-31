1eea8a7cc59e2ae77b7916ee52f82007d5128b5a
Alcatraz1191 <shrygdkr@gmail.com>
2024-10-31 12:15:39 -0500

    test commit 2

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 func main() {
   fmt.Println("Hello! This is a test")
+  fmt.Println("Here's a random number: ", calculate())
+}
+
+func calculate() int {
+  return rand.Intn(5) + rand.Intn(4)
 }
